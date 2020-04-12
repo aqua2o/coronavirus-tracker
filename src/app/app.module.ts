cda3c4d36084fab154791b5c9e35c3659569544c
aqua2o <mioarchivio2@gmail.com>
2020-04-12 18:48:38 +0100

    Added first routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { CountriesTableComponent } from './components/countries-table/countries-table.component';
 import { MatTableModule } from '@angular/material';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
@@ -16,7 +15,7 @@ import { CountriesService } from './services/contries.service';
 @NgModule({
   declarations: [
     AppComponent,
-    CountriesTableComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
@@ -26,7 +25,7 @@ import { CountriesService } from './services/contries.service';
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
   ],
   providers: [CountriesService],
   bootstrap: [AppComponent]

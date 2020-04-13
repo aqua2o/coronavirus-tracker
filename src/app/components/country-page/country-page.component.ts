9bbe59f29fca1b82d8f8acd17543b7d16260cd28
aqua2o <mioarchivio2@gmail.com>
2020-04-13 09:47:53 +0100

    Created first data structure for dates

--- src/app/components/country-page/country-page.component.ts
+++ src/app/components/country-page/country-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CountryService } from '../../services/country.service';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-country-page',
@@ -15,7 +16,19 @@ export class CountryPageComponent implements OnInit {
     console.log(this.route.snapshot.paramMap.get('country'));
     const country = this.route.snapshot.paramMap.get('country');
     this.countryService.getCountryInfo(country).subscribe((response) => {
-      console.log('response', response);
+      console.log('response country page', response);
+      let max = 9000;
+      let min = 0;
+      let allDates = response.map(res => res.Date);
+      console.log('all dates', allDates);
+
+      let countryDates = [];
+      allDates.forEach(res => {
+        let jsdate = new Date(res);
+        countryDates.push(jsdate.toLocaleTimeString('en', {year: 'numeric', month: 'short', day: 'numeric'}));
+      });
+
+      console.log('countryDates', countryDates);
     });
   }
 }
